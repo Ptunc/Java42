c5af1b17d198cb4acef83f68aef5afbab652d0ad
Ptunc <spolentunc@gmail.com>
2022-11-02 10:15:44 +0300

    just commit

---
+++ java42.java
@@ -0,0 +1,40 @@
+package Java42;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class java42 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] dizi = new int[6];
+        System.out.println("6 sayı girin: ");
+        for(int i=0; i<6; i++){
+            System.out.print(i+1 + ". sayı: ");
+            int a = input.nextInt();
+            dizi[i] = a;
+        }
+        Arrays.sort(dizi);
+        int min = dizi[0], max = dizi[5];
+        System.out.println("referans: ");
+        int b = input.nextInt();
+        if(b <= dizi[3]){
+            for(int i = 0; i<dizi.length/2; i++){
+                if(dizi[i] >= min && dizi[i] <= b) min = dizi[i];
+                if(dizi[i] <= max && dizi[i] >= b) max = dizi[i];
+            }
+            System.out.println("min: " + min);
+            System.out.println("max: " + max);
+        }
+        if(b >= dizi[3]){
+            for(int i = 3; i<dizi.length; i++){
+                if(dizi[i] >= min && dizi[i] <= b) min = dizi[i];
+                if(dizi[i] <= max && dizi[i] >= b) max = dizi[i];
+            }
+            System.out.println("min: " + min);
+            System.out.println("max: " + max);
+        }
+
+        input.close();
+    }
+    
+}
